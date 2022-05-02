13488d2487786b202e827ed528450085d373e46e
DmytroMoisiuk <47913030+DmytroMoisiuk@users.noreply.github.com>
2022-05-02 11:36:57 +0300

    Update Python_3.7_Dahua_07.05.19.py

--- Python_3.7_Dahua_07.05.19.py
+++ Python_3.7_Dahua_07.05.19.py
@@ -13,20 +13,44 @@ import sys
 import hashlib
 import re
 import binascii
+import wmi
 
 
 def Time_conv(Time):
     Time_List = ['Time']
     Time_List.append(hex(Time))
-    c = int(Time_List[1][4],16)
-    d = int(Time_List[1][5],16)
-    b = int(Time_List[1][3],16)
-    a = int(Time_List[1][2],16)
-    d = int(Time_List[1][5],16)
-    e = int(Time_List[1][6], 16)
-    f = int(Time_List[1][7], 16)
-    g = int(Time_List[1][8], 16)
-    h = int(Time_List[1][9], 16)
+    try:
+        c = int(Time_List[1][4],16)
+    except:
+       print(error) 
+    try:
+        d = int(Time_List[1][5],16)
+    except:
+        print(error)
+    try:
+        b = int(Time_List[1][3],16)
+    except:
+        print(error)
+    try:
+        a = int(Time_List[1][2],16)
+    except:
+        print(error)
+    try:    
+        e = int(Time_List[1][6],16)
+    except:
+        print(error)    
+    try:
+        f = int(Time_List[1][7],16)
+    except:
+        print(error)
+    try:
+        g = int(Time_List[1][8],16)
+    except:
+        print(error)
+    try:
+        h = int(Time_List[1][9],16)
+    except:
+        print(error)
     #b = int(b,16)
     #print(b)
     if (((c % 4)*8 + d // 2))<10:
@@ -58,7 +82,7 @@ def Time_conv(Time):
   #  print(data)
     return data
 
-def carve_file(f,blocksize,quality,Spath):  #blocksize-розмір блоку для аналізу; quality- якість
+def carve_file(f,blocksize,quality,Spath):
     SignStr = b'\x44\x48\x41\x56\xFD'
 #    SignStr = b'\x44\x48\x41\x56'
     SignEnd = b'\x64\x68\x61\x76'
@@ -92,14 +116,14 @@ def carve_file(f,blocksize,quality,Spath):  #blocksize-розмір блоку �
             if i == 0:
                 StartOffset = offsetSt
                 FirstCam = int.from_bytes(buf[offsetSt+6:offsetSt+8],byteorder='little')+1
-                FirstDate_ = int.from_bytes((buf[offsetSt + 16:offsetSt + 20]),byteorder='little')  # зчитування в int little endian
-                FirstDate = int.from_bytes((buf[offsetSt + 16:offsetSt + 20]),byteorder='little') # зчитування в int little endian
+                FirstDate_ = int.from_bytes((buf[offsetSt + 16:offsetSt + 20]),byteorder='little')  # int little endian
+                FirstDate = int.from_bytes((buf[offsetSt + 16:offsetSt + 20]),byteorder='little') # int little endian
                 FirstQual = int.from_bytes(buf[offsetSt + 29:offsetSt + 30],byteorder='big')
                 i = 1
             else:
                 cam = int.from_bytes(buf[offsetSt + 6:offsetSt + 8], byteorder='little') + 1
 
-                dateK = int.from_bytes((buf[offsetSt + 16:offsetSt + 20]),byteorder='little') # зчитування в int little endian
+                dateK = int.from_bytes((buf[offsetSt + 16:offsetSt + 20]),byteorder='little') #int little endian
                 delta = dateK - FirstDate
                 Qual = int.from_bytes(buf[offsetSt + 29:offsetSt + 30],byteorder = 'big')
 #                print (delta)
@@ -111,11 +135,17 @@ def carve_file(f,blocksize,quality,Spath):  #blocksize-розмір блоку �
                         EndOffset = offsetSt-1
                         subdata = buf[StartOffset:EndOffset]
                         if (FirstQual == quality) or (quality == all):
-                            time_s = Time_conv(FirstDate)
+                            try:
+                                time_s = Time_conv(FirstDate)
+                            except:
+                                time_s = '000000'
                             #time_e = Time_conv(FirstDate)
 
                             #filename = "N:\start_"+ time_s + '_' + str(int(l * blocksize + StartOffset)) + "_" + str(int(l * blocksize + EndOffset)) + "_" + "Cam_" + str(FirstCam) + '.dav'
-                            filename = Spath +  "\Cam_"+ str(FirstCam)+ '_' + Time_conv(FirstDate_) + '-'+ Time_conv(FirstDate)+'_' + str(int(l * blocksize + StartOffset)) + "_" + str(int(l * blocksize + EndOffset)) + '.dav'
+                            try:
+                                filename = Spath +  "\Cam_"+ str(FirstCam)+ '_' + Time_conv(FirstDate_) + '-'+ Time_conv(FirstDate)+'_' + str(int(l * blocksize + StartOffset+jump)) + "_" + str(int(l * blocksize + EndOffset+jump)) + '.dav'
+                            except:
+                                filename = Spath +  "\Cam_"+ str(FirstCam)+ '_' + '000000' + '-'+ '000000'+'_' + str(int(l * blocksize + StartOffset+jump)) + "_" + str(int(l * blocksize + EndOffset+jump)) + '.dav'
 
                             copy_file = open(filename,'wb')
                             copy_file.write(subdata)
@@ -127,8 +157,8 @@ def carve_file(f,blocksize,quality,Spath):  #blocksize-розмір блоку �
                             print(filename)
                         StartOffset = offsetSt
                         FirstCam = int.from_bytes(buf[offsetSt + 6:offsetSt + 8], byteorder='little') + 1
-                        FirstDate = int.from_bytes((buf[offsetSt + 16:offsetSt + 20]),byteorder='little')  # зчитування в int little endian
-                        FirstDate_ = int.from_bytes((buf[offsetSt + 16:offsetSt + 20]),byteorder='little')  # зчитування в int little endian
+                        FirstDate = int.from_bytes((buf[offsetSt + 16:offsetSt + 20]),byteorder='little')  #int little endian
+                        FirstDate_ = int.from_bytes((buf[offsetSt + 16:offsetSt + 20]),byteorder='little')  #int little endian
                         FirstQual = int.from_bytes(buf[offsetSt + 29:offsetSt + 30], byteorder='big')
                         i = 1
                         k = k + 1
@@ -137,7 +167,7 @@ def carve_file(f,blocksize,quality,Spath):  #blocksize-розмір блоку �
         if (c == 0)and (StartOffset != 0) and (EndOffset != 0) :
             if (FirstQual == quality) or (quality == all):
                 subdata = buf[StartOffset:EndOffset]
-                filename = Spath +  "\Cam_"+ str(FirstCam)+ '_' + Time_conv(FirstDate_)+ '-'+ Time_conv(FirstDate)+'_' + str(int(l * blocksize + StartOffset)) + "_" + str(int(l * blocksize + EndOffset)) +  '.dav'
+                filename = Spath +  "\Cam_"+ str(FirstCam)+ '_' + Time_conv(FirstDate_)+ '-'+ Time_conv(FirstDate)+'_' + str(int(l * blocksize + StartOffset+jump)) + "_" + str(int(l * blocksize + EndOffset+jump)) +  '.dav'
                 copy_file = open(filename, 'wb')
                 copy_file.write(subdata)
                 copy_file.close()
@@ -149,16 +179,45 @@ def carve_file(f,blocksize,quality,Spath):  #blocksize-розмір блоку �
 #        print (Time_List)
 #        print (Qual_List)
         print('chunk_' + str(l))
-        print('Скопійовано '+str(k)+ ' файл(-ів)')
+        print('Copy '+str(k)+ ' -s')
     return m.hexdigest()
 #    return SOF_List
 
+'''you need to make changes '''
+
+
+#************* Start source block***************#
+
+Disk_List = ['Disk List']
+Disk_info = ['Disk info']
+c = wmi.WMI()
+for diskDrive in c.query("SELECT * FROM Win32_DiskDrive"):
+    Disk_List.append(diskDrive.Name)
+    Disk_info.append(diskDrive.model)
+#    print(Disk_List)
+l = 1
+print("Available drives:")
+while l < len(Disk_List):
+    print(l, ".", Disk_List[l]," Model:",Disk_info[l])
+    l += 1
+cmd = input("Enter number: ")    
  
-#f=open('\\\\.\\PhysicalDrive7','rb') # Work with connected Drive
-f=open('J:\Test_2.001','rb')  #Work with RAW image 
-Spath = "D:\Video" # Destination path
+#f=open('\\\\.\\PhysicalDrive1','rb') #  Work with connected Drive
+f=open(Disk_List[int(cmd)],'rb')
+#f=open('L:\TOSHIBA HDWD110.001','rb')    # Work with RAW image  
+
+#****************End source block*******************#
+
+
+print("Enter destination path:   Example: H:\Video")
+cmd = input("Enter path: ")    
+Spath = cmd
+#Spath = "H:\Video_Ez-IP" # Destination path   
+print(Spath)
+
 jump=0 # bytes multiple sector size (example:1174305148928)
 f.seek(jump,0) #move the file pointer forward by jump bytes from start of the file
+print('jump to: ', jump ,'  ok')
 blocksize=2**30  # Block size
 quality = all   # Quality of video frames. For all type of quality use 'all' without quotation marks.
 
